884d3a70a57d4bb1e645cbdedc428b99aa2188c7
Slotscowboy <39527327+Slotscowboy@users.noreply.github.com>
2024-03-20 08:25:30 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
-// Функция для отправки запросов к API сервиса геолокации
+// Функция для отправки запроса к API сервиса ip.me и извлечения кода страны
 function getCountryByIP($ip) {
-    $url = "http://ip-api.com/json/$ip";
+    $url = "https://ip.me";
     $response = file_get_contents($url);
-    return json_decode($response, true);
+    preg_match('/Country Code: ([A-Z]+)/', $response, $matches);
+    if (isset($matches[1])) {
+        return $matches[1];
+    } else {
+        return null;
+    }
 }
 
 // Получение IP-адреса пользователя
 $ip = $_SERVER['REMOTE_ADDR'];
 
 // Определение страны пользователя по IP-адресу
-$data = getCountryByIP($ip);
-$country = $data['countryCode'];
+$country = getCountryByIP($ip);
 
-// Если пользователь из России, редиректим по первым 3 ссылкам
-if ($country == "RU") {
-    $lines = file("https://sext.netlify.app/links.txt");
-    for ($i = 0; $i < 3; $i++) {
-        header("Location: " . trim($lines[$i]));
+// Если удалось получить код страны
+if ($country) {
+    // Если пользователь из России, редиректим по первым 3 ссылкам
+    if ($country == "RU") {
+        $lines = file("https://sext.netlify.app/links.txt");
+        for ($i = 0; $i < 3; $i++) {
+            header("Location: " . trim($lines[$i]));
+            exit; // Останавливаем выполнение скрипта после редиректа
+        }
+    }
+    // Иначе редиректим на четвертую ссылку
+    else {
+        $lines = file("https://sext.netlify.app/links.txt");
+        header("Location: " . trim($lines[3]));
         exit; // Останавливаем выполнение скрипта после редиректа
     }
-}
-// Иначе редиректим на четвертую ссылку
-else {
-    $lines = file("https://sext.netlify.app/links.txt");
-    header("Location: " . trim($lines[3]));
-    exit; // Останавливаем выполнение скрипта после редиректа
+} else {
+    // Если не удалось получить код страны, выводим сообщение об ошибке
+    echo "Не удалось определить местоположение пользователя.";
 }
 ?>
