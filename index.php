73de41accd1e00f614cb70ddf46d5f4b55cf1d55
Slotscowboy <39527327+Slotscowboy@users.noreply.github.com>
2024-03-20 09:27:28 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
-// Получение IP-адреса пользователя
+// Функция для определения местоположения пользователя по IP-адресу
+function getCountryByIP($ip) {
+    $response = file_get_contents("https://ipwhois.app/json/$ip");
+    $data = json_decode($response, true);
+    return isset($data['country_code']) ? $data['country_code'] : null;
+}
+
+// Получаем IP-адрес пользователя
 $ip = $_SERVER['REMOTE_ADDR'];
 
-// Отправка запроса к сервису GeoIP для определения местоположения пользователя
-$response = file_get_contents("https://get.geojs.io/v1/ip/country/$ip.json");
-$data = json_decode($response, true);
+// Получаем код страны пользователя
+$countryCode = getCountryByIP($ip);
 
-// Получение кода страны
-$countryCode = isset($data['country']) ? $data['country'] : null;
+// Читаем ссылки из файла links.txt
+$links = file("links.txt", FILE_IGNORE_NEW_LINES);
 
-// Вывод кода страны
-if ($countryCode) {
-    echo $countryCode; // Вывод кода страны
+// Если пользователь из России, редиректим на одну из первых трех ссылок случайным образом
+if ($countryCode === "RU") {
+    $randomIndex = array_rand($links, 3);
+    $redirectUrl = $links[$randomIndex[array_rand($randomIndex)]];
 } else {
-    echo "Не удалось определить местоположение пользователя.";
+    // Иначе редиректим на четвертую ссылку
+    $redirectUrl = $links[3];
 }
+
+// Выполняем редирект на полученную ссылку
+header("Location: $redirectUrl");
+exit;
 ?>
