5ed48d39eb372304f6721cad4ce7fac0cd7abef7
Slotscowboy <39527327+Slotscowboy@users.noreply.github.com>
2024-03-20 10:24:08 +0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,15 @@
 window.onload = function() {
+    // Функция для определения местоположения пользователя по IP-адресу
+    function getCountryByIP() {
+        return fetch('https://ipwhois.app/json/')
+            .then(response => response.json())
+            .then(data => data.country_code)
+            .catch(error => {
+                console.error('Error fetching country:', error);
+                return null;
+            });
+    }
+
     // Читаем ссылки из файла links.txt
     fetch('links.txt')
         .then(response => response.text())
@@ -7,12 +18,20 @@ window.onload = function() {
             // Получаем текущий индекс строки, на которую должен будет редиректиться пользователь
             let redirectIndex = localStorage.getItem('redirectIndex');
             redirectIndex = redirectIndex ? parseInt(redirectIndex) : 0;
-            // Следующая строка в кольцевом порядке
-            redirectIndex = (redirectIndex + 1) % links.length;
-            // Сохраняем индекс для следующего раза
-            localStorage.setItem('redirectIndex', redirectIndex);
-            // Выполняем редирект на полученную ссылку
-            window.location.href = links[redirectIndex];
+            // Получаем код страны пользователя и решаем, должен ли произойти редирект
+            getCountryByIP().then(countryCode => {
+                if (countryCode === 'RU') {
+                    // Если пользователь из России, то редиректим его на одну из первых трех ссылок
+                    redirectIndex = redirectIndex % 3; // Ограничиваем индекс до первых трех ссылок
+                } else {
+                    // Если пользователь не из России, редиректим его всегда на 4 ссылку
+                    redirectIndex = 3;
+                }
+                // Сохраняем индекс для следующего раза
+                localStorage.setItem('redirectIndex', redirectIndex);
+                // Выполняем редирект на полученную ссылку
+                window.location.href = links[redirectIndex];
+            });
         })
         .catch(error => console.error('Error fetching links:', error));
 };
